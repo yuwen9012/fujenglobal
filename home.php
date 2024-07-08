dd6845c03410dc126b68fdeff1ab43e14475d6a3
yuwen9012 <156352342+yuwen9012@users.noreply.github.com>
2024-07-09 00:33:54 +0800

    Add files via upload

---
+++ home.php
@@ -0,0 +1,206 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+
+    <!-- icon -->
+    <link href="images/logo.ico" rel="shortcut icon" />
+
+    <!-- Bootstrap -->
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-YvpcrYf0tY3lHB60NNkmXc5s9fDVZLESaAA55NDzOxhy9GkcIdslK1eN7N6jIeHz" crossorigin="anonymous"></script>
+
+    <!-- FontAwesome CSS -->
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.4/css/all.min.css">
+
+    <link rel="stylesheet" href="css/home.css">
+    <title>輔仁大學國際及兩岸教育處</title>
+</head>
+<body>
+    <nav class="navbar navbar-expand-lg bg-body-tertiary">
+        <div class="container-fluid">
+                
+            <div class="navbar-nav ms-auto d-flex align-items-center">
+                <div class="nav-item d-flex align-items-center me-3">
+                    <i class="fas fa-home me-1"></i>
+                    <a class="nav-link" href="#">Home</a>
+                </div>
+                <div class="nav-item d-flex align-items-center me-3">
+                    <i class="fas fa-language me-1"></i>
+                    <a class="nav-link" href="#">Lang</a>
+                </div>
+                <div class="nav-item d-flex align-items-center me-3">
+                    <i class="fas fa-university me-1"></i>
+                    <a class="nav-link" href="#">FJCU</a>
+                </div>
+                <div class="nav-item d-flex align-items-center me-3">
+                    <i class="fas fa-sitemap me-1"></i>
+                    <a class="nav-link" href="#">Sitemap</a>
+                </div>
+            </div>
+
+        </div>
+    </nav>
+
+    <nav class="navbar navbar-expand-lg bg-body-tertiary second-navbar">
+        <div class="container-fluid">
+            <img src="images/06-校徽-英文組合排列.png" id="logo">
+            
+                
+            <ul class="navbar-nav ms-auto">
+                <li class="nav-item">
+                    <span class="navbar-text text-white mx-2">|</span>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link text-white" href="#">Fu Jen Global Academy</a>
+                </li>
+                <li class="nav-item">
+                    <span class="navbar-text text-white mx-2">|</span>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link text-white" href="#">Degree students</a>
+                </li>
+                <li class="nav-item">
+                    <span class="navbar-text text-white mx-2">|</span>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link text-white" href="#">Study Aboard</a>
+                </li>
+                <li class="nav-item">
+                    <span class="navbar-text text-white mx-2">|</span>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link text-white" href="#">Chinese Student</a>
+                </li>
+                <li class="nav-item">
+                    <span class="navbar-text text-white mx-2">|</span>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link text-white" href="#">Enrollment Information</a>
+                </li>
+                <li class="nav-item">
+                    <span class="navbar-text text-white mx-2">|</span>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link text-white" href="#">Contact us</a>
+                </li>
+            </ul>
+
+        </div>
+    </nav>
+
+    <div class="container">
+        <div id="carouselExampleIndicators" class="carousel slide" data-ride="carousel">
+            <div class="carousel-indicators">
+                <button type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide-to="0" class="active" aria-current="true" aria-label="Slide 1"></button>
+                <button type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide-to="1" aria-label="Slide 2"></button>
+                <button type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide-to="2" aria-label="Slide 3"></button>
+            </div>
+            <div class="carousel-inner">
+                <div class="carousel-item active">
+                    <img src="images/banner1.png" class="d-block w-100" alt="...">
+                    <div class="overlay">
+                        <h3>Internal Education</h3>
+                    </div>
+                </div>
+                <div class="carousel-item">
+                    <img src="images/banner1.png" class="d-block w-100" alt="...">
+                </div>
+                <div class="carousel-item">
+                    <img src="images/banner1.png" class="d-block w-100" alt="...">
+                </div>
+            </div>
+            <button class="carousel-control-prev" type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide="prev">
+                <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+                <span class="visually-hidden">Previous</span>
+            </button>
+            <button class="carousel-control-next" type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide="next">
+                <span class="carousel-control-next-icon" aria-hidden="true"></span>
+                <span class="visually-hidden">Next</span>
+            </button>
+        </div>
+    </div>
+
+    <div class="container my-5">
+        <div class="row">
+            <div class="col-md-6">
+                <div class="p-3">
+                    <h2>Fu Jen Catholic University</h2>
+                    <p>is renowned for its robust international exchange programs and has established partnerships with numerous universities worldwide. Students have opportunities to participate in various international exchange and internship programs, broadening their global perspectives.</p>
+                    <button type="button" class="btn btn-primary">Life Guide</button>
+                </div>
+            </div>
+            <div class="col-md-6">
+                <div class="row gx-4 gy-4">
+                    <div class="col-md-6">
+                        <img src="images/classroom  (9).jpg" class="img-fixed-height rounded" alt="Image 1">
+                    </div>
+                    <div class="col-md-6">
+                        <img src="images/投影牆1.jpg" class="img-fixed-height rounded" alt="Image 2">
+                    </div>
+                    <div class="col-md-6">
+                        <img src="images/classroom  (19).jpg" class="img-fixed-height rounded" alt="Image 3">
+                    </div>
+                    <div class="col-md-6">
+                        <img src="images/野聲樓.jpg" class="img-fixed-height rounded" alt="Image 4">
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+
+    <footer class="text-white py-4">
+        <div class="container">
+            <div class="row">
+                <div class="col-md-3 text-center">
+                    <i class="fas fa-map-marker-alt"></i>
+                    <h5>Location</h5>
+                    <ul class="list-unstyled">
+                        <p>Office of International Education Room 112, Cardinal Tien Memorial Hall 510 Zhongzheng Rd., Xinzhuang Dist.,Taipei, Taiwan 242062</p>
+                    </ul>
+                </div>
+                <div class="col-md-3 text-center with-divider">
+                    <i class="fas fa-link"></i>
+                    <h5>Link</h5>
+                    <ul class="list-unstyled">
+                        <li><a href="https://www.fju.edu.tw/indexEN.jsp">https://www.fju.edu.tw/indexEN.jsp</a></li>
+                        <li><a href="https://www.fju.edu.tw/indexEN.jsp">https://www.fju.edu.tw/indexEN.jsp</a></li>
+                        <li><a href="https://www.fju.edu.tw/indexEN.jsp">https://www.fju.edu.tw/indexEN.jsp</a></li>
+                    </ul>
+                </div>
+                <div class="col-md-3 text-center with-divider">
+                    <i class="fas fa-question-circle"></i>
+                    <h5>Q & A</h5>
+                    <ul class="list-unstyled">
+                        <p>Any questions about us ?</p>
+                        <p>Please click here !</p>
+                        <button type="button" class="btn btn-primary"><i class="fas fa-location-arrow"></i></button>
+                    </ul>
+                </div>
+                <div class="col-md-3 text-center with-divider">
+                    <i class="fas fa-envelope"></i>
+                    <h5>Contact number</h5>
+                    <ul class="list-unstyled">
+                        <p>General Inquiry</p>
+                        <p>+886-2-2905-3137</p>
+                        <P>Global Partnership</p>
+                        <p>+886-2-2905-3358</p>
+                        <p>International Student Admission</p>
+                        <p>+886-2-2905-2944</p>
+                        <p>Chinese Language</p>
+                        <p>+886-2-2905-3721</p>
+                    </ul>
+                </div>
+            </div>
+
+            <div class="row mt-4">
+                <div class="col text-center">
+                    <img src="images/05-校徽-英文組合排列.png" alt="Logo" class="img-fluid" style="max-height: 50px;">
+                </div>
+            </div>
+        </div>
+    </footer>
+
+</body>
+</html>
