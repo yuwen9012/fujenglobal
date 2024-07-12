92eee26c7919fc7e29be158ff34e564f22d2e95c
Justin <justin.official.2022@gmail.com>
2024-07-12 11:43:50 +0800

    改字型

--- home.php
+++ home.php
@@ -7,6 +7,9 @@
     <!-- icon -->
     <link href="images/logo.ico" rel="shortcut icon" />
 
+    <!-- 引入 Poppins 字體 -->
+    <link rel="stylesheet" href="https://fonts.googleapis.com/css2?family=Poppins:wght@400;600&display=swap">
+
     <!-- Bootstrap -->
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-YvpcrYf0tY3lHB60NNkmXc5s9fDVZLESaAA55NDzOxhy9GkcIdslK1eN7N6jIeHz" crossorigin="anonymous"></script>
@@ -14,7 +17,9 @@
     <!-- FontAwesome CSS -->
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.4/css/all.min.css">
 
+    <!-- CSS -->
     <link rel="stylesheet" href="css/home.css">
+
     <title>輔仁大學國際及兩岸教育處</title>
 </head>
 <body>
@@ -53,7 +58,7 @@
                     <span class="navbar-text text-white mx-2">|</span>
                 </li>
                 <li class="nav-item">
-                    <a class="nav-link text-white" href="#">Fu Jen Global Academy</a>
+                    <a class="nav-link text-white" href="https://bpis.fju.edu.tw/index.php">Fu Jen Global Academy</a>
                 </li>
                 <li class="nav-item">
                     <span class="navbar-text text-white mx-2">|</span>
