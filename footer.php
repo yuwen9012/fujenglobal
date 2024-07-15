33b23940051ffcdd8444fd24c57c65efe6f20bca
Justin <justin.official.2022@gmail.com>
2024-07-15 14:36:21 +0800

    footer 刪除註解

--- footer.php
+++ footer.php
@@ -1,28 +1,24 @@
 <footer class="py-3">
-    <!-- <div class="container"> -->
-        <div class="row mx-4 my-2">
-            <!-- <div class="col d-flex justify-content-between"> -->
-                <div class="col-4 d-flex flex-column justify-content-center text-start fs-6">
-                    <span class="text-white g-1">
-                        <i class="fas fa-map-marker-alt"></i>
-                        <label>Office of International Education, Room 112, Cardinal Tien Memorial Hall</label>
-                        <label>510 Zhongzheng Rd., Xinzhuang Dist., Taipei, Taiwan 242062</label>
-                    </span>
-                    <span class="text-white g-1">
-                        <i class="fas fa-phone"></i>
-                        <label>02-2345-6789</label>
-                    </span>
-                </div>
-
-                <div class="col-4 d-flex align-items-center justify-content-center text-center">
-                    <img src="images/05-校徽-英文組合排列.png" alt="Logo" class="img-fluid" style="max-height: 80px;">
-                </div>
-
-                <div class="col-4 d-flex flex-column justify-content-center text-end">
-                    <span class="text-white fs-9">製作: 陳郁雯、郭政億、王孟蘋</span>
-                    <span class="text-white fs-9">指導: 輔仁大學統計資訊學系 杜逸寧 副教授</span>
-                </div>
-            <!-- </div> -->
+    <div class="row mx-4 my-2">
+        <div class="col-4 d-flex flex-column justify-content-center text-start fs-6">
+            <span class="text-white g-1">
+                <i class="fas fa-map-marker-alt"></i>
+                <label>Office of International Education, Room 112, Cardinal Tien Memorial Hall</label>
+                <label>510 Zhongzheng Rd., Xinzhuang Dist., Taipei, Taiwan 242062</label>
+            </span>
+            <span class="text-white g-1">
+                <i class="fas fa-phone"></i>
+                <label>02-2345-6789</label>
+            </span>
         </div>
-    <!-- </div> -->
+
+        <div class="col-4 d-flex align-items-center justify-content-center text-center">
+            <img src="images/05-校徽-英文組合排列.png" alt="Logo" class="img-fluid" style="max-height: 80px;">
+        </div>
+
+        <div class="col-4 d-flex flex-column justify-content-center text-end">
+            <span class="text-white fs-9">製作: 陳郁雯、郭政億、王孟蘋</span>
+            <span class="text-white fs-9">指導: 輔仁大學統計資訊學系 杜逸寧 副教授</span>
+        </div>
+    </div>
 </footer>
