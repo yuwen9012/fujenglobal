55b70bcccb74be9f8b12067c3b2b5a64b0da77d7
yuwen9012 <156352342+yuwen9012@users.noreply.github.com>
2024-07-15 16:10:04 +0800

    刪掉第三段留footer

--- home.php
+++ home.php
@@ -131,52 +131,6 @@
             </div>
         </div>
     </div>
-
-    <footer class="text-white pt-4">
-        
-            <div class="row">
-                <div class="col-md-3 text-center">
-                    <i class="fas fa-map-marker-alt"></i>
-                    <h5>Location</h5>
-                    <ul class="list-unstyled">
-                        <p>Office of International Education Room 112, Cardinal Tien Memorial Hall 510 Zhongzheng Rd., Xinzhuang Dist.,Taipei, Taiwan 242062</p>
-                    </ul>
-                </div>
-                <div class="col-md-3 text-center with-divider">
-                    <i class="fas fa-link"></i>
-                    <h5>Link</h5>
-                    <ul class="list-unstyled">
-                        <li><a href="https://www.fju.edu.tw/indexEN.jsp">https://www.fju.edu.tw/indexEN.jsp</a></li>
-                        <li><a href="https://www.fju.edu.tw/indexEN.jsp">https://www.fju.edu.tw/indexEN.jsp</a></li>
-                        <li><a href="https://www.fju.edu.tw/indexEN.jsp">https://www.fju.edu.tw/indexEN.jsp</a></li>
-                    </ul>
-                </div>
-                <div class="col-md-3 text-center with-divider">
-                    <i class="fas fa-question-circle"></i>
-                    <h5>Q & A</h5>
-                    <ul class="list-unstyled">
-                        <p>Any questions about us ?</p>
-                        <p>Please click here !</p>
-                        <button type="button" class="btn btn-primary"><i class="fas fa-location-arrow"></i></button>
-                    </ul>
-                </div>
-                <div class="col-md-3 text-center with-divider">
-                    <i class="fas fa-envelope"></i>
-                    <h5>Contact number</h5>
-                    <ul class="list-unstyled">
-                        <p>General Inquiry</p>
-                        <p>+886-2-2905-3137</p>
-                        <P>Global Partnership</p>
-                        <p>+886-2-2905-3358</p>
-                        <p>International Student Admission</p>
-                        <p>+886-2-2905-2944</p>
-                        <p>Chinese Language</p>
-                        <p>+886-2-2905-3721</p>
-                    </ul>
-                </div>
-            </div>
-    </footer>
-
     <?php include 'footer.php'; ?>
 </body>
 </html>
