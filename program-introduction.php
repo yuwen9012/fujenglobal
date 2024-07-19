b4013679757ebd61314bcf015f23cd6a88d4a3ac
yuwen9012 <156352342+yuwen9012@users.noreply.github.com>
2024-07-19 23:17:36 +0800

    Add files via upload

---
+++ program-introduction.php
@@ -0,0 +1,75 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+
+    <!-- icon -->
+    <link href="images/logo.ico" rel="shortcut icon" />
+
+    <!-- 引入 Poppins 字體 -->
+    <link rel="stylesheet" href="https://fonts.googleapis.com/css2?family=Poppins:wght@400;600&display=swap">
+
+    <!-- Bootstrap -->
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-YvpcrYf0tY3lHB60NNkmXc5s9fDVZLESaAA55NDzOxhy9GkcIdslK1eN7N6jIeHz" crossorigin="anonymous"></script>
+
+    <!-- FontAwesome CSS -->
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.5.1/css/all.min.css">
+
+    <!-- CSS -->
+    <link rel="stylesheet" href="css/study-package.css">
+
+    <!-- JS -->
+    <script type="text/javascript" src="js/study-package.js"></script>
+
+    <title>Program Introduction</title>
+</head>
+<body>
+    <?php include 'header.php'; ?>
+
+    <nav class="navbar navbar-expand-lg bg-body-tertiary second-navbar">
+        <div class="container-fluid d-flex align-items-center">
+            <img src="images/06-校徽-英文組合排列.png" id="logo" class="me-3">
+            
+            <div class="flex-grow-1 text-center">
+                <div class="d-flex justify-content-center align-items-center">
+                    <a href="study-abroad.php"><i class="fas fa-arrow-left"></i></a>
+                    <span class="navbar-text mx-2">Program Introduction</span>
+                </div>
+            </div>
+        </div>
+    </nav>
+
+     <div class="container-fluid introduction-block">
+        <div class="row">
+            <div class="col-md-1 left-section">
+            </div>
+
+            <div class="col-md-10 center-section">
+                <img src="images/program1.jpg" alt="Description of the image" class="img-fluid">
+            </div>
+
+
+            <div class="col-md-1 right-section">
+            </div>
+        </div>
+    </div>
+
+      <div class="container-fluid introduction-block">
+        <div class="row">
+            <div class="col-md-1 left-section">
+            </div>
+
+            <div class="col-md-10 center-section">
+                <img src="images/program2.jpg" alt="Description of the image" class="img-fluid">
+            </div>
+
+
+            <div class="col-md-1 right-section">
+            </div>
+        </div>
+    </div>
+    <?php include 'footer.php'; ?>
+<body>
+</html>
