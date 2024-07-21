256c5c00a70edcecb12c369e05d8d6af1680951e
dreamapple01 <dreamapple2003@gmail.com>
2024-07-21 19:13:12 +0800

    A改大寫

--- howtoapply.php
+++ howtoapply.php
@@ -37,7 +37,7 @@
             <div class="flex-grow-1 text-center">
                 <div class="d-flex justify-content-center align-items-center">
                     <a href="study-abroad.php"><i class="fas fa-arrow-left"></i></a>
-                    <span class="navbar-text mx-2">How to apply</span>
+                    <span class="navbar-text mx-2">How to Apply</span>
                 </div>
             </div>
         </div>
