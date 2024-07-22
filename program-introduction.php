4923e83aadf6a3c9ea192a53aca7db6aaefd6510
yuwen9012 <156352342+yuwen9012@users.noreply.github.com>
2024-07-22 15:01:05 +0800

    引用footer的css

--- program-introduction.php
+++ program-introduction.php
@@ -18,6 +18,7 @@
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.5.1/css/all.min.css">
 
     <!-- CSS -->
+    <link rel="stylesheet" href="css/fixed.css">
     <link rel="stylesheet" href="css/study-package.css">
 
     <!-- JS -->
