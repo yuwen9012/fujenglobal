4be4eba2fa2f36cbc6c9f30e045af4e459924f52
yuwen9012 <156352342+yuwen9012@users.noreply.github.com>
2024-07-28 14:41:37 +0800

    連接Mandarin-Chinese-Learning.php

--- study-abroad.php
+++ study-abroad.php
@@ -60,7 +60,7 @@
                         <div class="circle-btn" id="btn-study-package"><a href="study-package.php">Study Package</a></div>
                         <div class="circle-btn" id="btn-traditional-culture"><a href="#">Traditional Culture</a></div>
                         <div class="circle-btn" id="btn-enterprise-visit"><a href="https://bpis.fju.edu.tw/index.php">Enterprise Visit</a></div>
-                        <div class="circle-btn" id="btn-mandarin-learning"><a href="#">Mandarin Learning</a></div>
+                        <div class="circle-btn" id="btn-mandarin-learning"><a href="Mandarin-Chinese-Learning.php">Mandarin Learning</a></div>
                         <img src="images/SA-student.png" id="student" class="d-block mx-auto" alt="student">
                     </div>
                 </div>
