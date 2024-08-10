afc4f2ba596658fed5d99fb75018e99d2f212d77
yuwen9012 <156352342+yuwen9012@users.noreply.github.com>
2024-08-10 21:41:42 +0800

    Art完成

---
+++ study-package/art.php
@@ -0,0 +1,191 @@
+<div class="tab-pane fade" id="art" role="tabpanel" aria-labelledby="art-tab">
+    <h3 class="my-3 fw-bolder">College of Art</h3>
+    
+    <ul class="navbar-nav d-flex flex-row flex-wrap">
+        <li class="nav-item m-1">
+            <a class="nav-link btn fs-5" href="#if-art" data-scroll="true">Introduction & Features</a>
+        </li>
+        <li class="nav-item m-1">
+            <a class="nav-link btn fs-5" href="#amsp-art" data-scroll="true">Academic Module & Study Package</a>
+        </li>
+        <li class="nav-item m-1">
+            <a class="nav-link btn fs-5" href="#lo-art" data-scroll="true">Learning Outcomes</a>
+        </li>
+        <li class="nav-item m-1">
+            <a class="nav-link btn fs-5" href="#ff-art" data-scroll="true">Faculty & Facilities</a>
+        </li>
+        <li class="nav-item m-1">
+            <a class="nav-link btn fs-5" href="#ow-art" data-scroll="true">Office Website</a>
+        </li>
+        <li class="nav-item ml-auto">
+            <a class="nav-link top back-to-top" href="#"><i class="fas fa-arrow-up"></i></a>
+        </li>
+    </ul>
+
+    <div class="my-5" id="if-art">
+        <div class="row align-items-start">
+            <div class="col-md-6 justify-text">
+                <h3 class="my-3 fw-semibold">Introduction & Features</h3>
+                <p class="my-3">The College of Arts at Fu Jen Catholic University combines the beauty of art into three departments: Music, Applied Art, and Landscape Architecture. They integrate the local and global perspectives, focusing on educational vision, community building, and placemaking while considering the SDGs (Sustainable Development Goals). The college has received numerous international art awards and aims to incorporate art therapy and artificial intelligence into human life. This will help expand the mind and creativity towards the development of the metaverse.</p>
+            </div>
+            <div class="col-md-6">
+                <img src="images/study-package/art/art1.png" class="container-fluid">
+            </div>
+        </div>
+    </div>
+
+    <div class="my-5" id="amsp-art">
+        <div class="row align-items-start">
+            <div class="col-md-6">
+                <img src="images/study-package/art/art2.jpg" class="container-fluid">
+            </div>
+            <div class="col-md-6 justify-text">
+                <h3 class="my-3 fw-semibold">Academic Module & Study Package</h3>
+                <p class="my-3 fw-semibold" style="font-size: 20px;">1. Department of Music</p>
+                <li class="mt-3 fw-semibold">Music Theory and Analysis:</li>
+                <span class="mt-3">Explore music theory, focusing on musical structures, harmonies, and composition.
+                </span>
+                <li class="mt-3 fw-semibold">Jazz:</li>
+                <span class="mt-3">Study jazz history, key figures, styles, and performance techniques.</span>
+                <div class="collapse" id="collapseArtAmsp">
+                    <li class="mt-3 fw-semibold">Applied Commercial Music:</li>
+                    <span class="mt-3">Practical training in commercial music production, including songwriting, recording, and marketing.
+                    </span>
+                    <li class="mt-3 fw-semibold">Music History:</li>
+                    <span class="mt-3">Survey music history, examining key periods, composers, and works.</span>
+                    <li class="mt-3 fw-semibold">Ethnomusicology Series Class in English:</li>
+                    <span class="mt-3">Courses on global music cultures, emphasizing fieldwork and ethnographic methods.</span>
+                    <li class="mt-3 fw-semibold">English for Music Performance and Presentation:</li>
+                    <span class="mt-3">Develop English skills for music performance and professional communication.</span>
+                    <li class="mt-3 fw-semibold">Academic Writing and Research Methodologies:</li>
+                    <span class="mt-3">Instruction in academic writing and research in music studies.</span>
+                    <li class="mt-3 fw-semibold">Professional Development and Industry Practices:</li>
+                    <span class="mt-3">Career guidance, including networking, portfolio development, and industry standards.</span>
+
+                    <p class="my-3 fw-semibold" style="font-size: 20px;">2. Department of Applied Art</p>
+                    <li class="mt-3 fw-semibold">Art Theory and Analysis in English:</li>
+                    <span class="mt-3">Examine art theories and critical analysis in English.</span>
+                    <li class="mt-3 fw-semibold">English for Art Presentation and Critique:</li>
+                    <span class="mt-3">Develop English skills for presenting and critiquing art.</span>
+                    <li class="mt-3 fw-semibold">Academic Writing and Research Methodologies:</li>
+                    <span class="mt-3">Techniques for academic writing and research in art.</span>
+                    <li class="mt-3 fw-semibold">Art History and Criticism:</li>
+                    <span class="mt-3">Survey art history and develop critical analysis skills.</span>
+                    <li class="mt-3 fw-semibold">Professional Development and Industry Practices:</li>
+                    <span class="mt-3">Career preparation in the art industry, including portfolio creation and networking.</span>
+
+                    <p class="my-3 fw-semibold" style="font-size: 20px;">3. Department of Landscape Architecture</p>
+                    <li class="mt-3 fw-semibold">Landscape Theory and Analysis in English: </li>
+                    <span class="mt-3">Study landscape architecture theories and analysis in English.</span>
+                    <li class="mt-3 fw-semibold">English for Landscape Presentation and Critique:</li>
+                    <span class="mt-3">Develop English skills for presenting and critiquing landscape projects.</span>
+                    <li class="mt-3 fw-semibold">Academic Writing and Research Methodologies:</li>
+                    <span class="mt-3">Instruction in academic writing and research for landscape architecture.</span>
+                    <li class="mt-3 fw-semibold">Environmental Design and Planning: </li>
+                    <span class="mt-3">Explore environmental design principles and sustainable planning.</span>
+                    <li class="mt-3 fw-semibold">Professional Development and Industry Practices:</li>
+                    <span class="mt-3">Career preparation for landscape architects, including portfolio building and industry standards.
+                    </span>
+                </div>
+                <a id="read-more-link-Art-Amsp" data-bs-toggle="collapse" href="#collapseArtAmsp" role="button" aria-expanded="false" aria-controls="collapseArtAmsp">Read more...</a>
+            </div>
+        </div>
+    </div>
+
+    <div class="my-5" id="lo-art">
+        <div class="row align-items-start">
+            <div class="col-md-6 justify-text">
+                <h3 class="my-3 fw-semibold">Learning Outcomes</h3>
+                <p class="my-3">The Music Department aims to develop internationally competitive music talents with the help of full-time international faculty. The curriculum covers a wide range of music genres from classical to contemporary, including courses in music theory, modern music therapy, and digital music applications. English courses and international exchanges are offered to enhance students' global competitiveness. Regular concerts, workshops, and masterclasses are organized to improve students' performance and creativity. Additionally, the department actively collaborates with renowned international institutions, providing students with more learning opportunities and enhancing its international reputation.</p>
+                <div class="collapse" id="collapseArtLo">
+                    <p class="my-3">Established in 1984, the Department of Applied Arts offers various courses in computer animation, metalworking, visual communication, and interior design. It focuses on high-quality, small-class teaching and expert faculty. The department established a Graduate Fine Arts program in 2001, which combines theory and practice. The Evening Division of the bachelor's degree program was introduced in 2010, offering tracks in Visual Communication and Metalworking. The department emphasizes strengthening moral ethics and creative thinking, integrating design theories with practice, cultivating local and international perspectives, and promoting group work and industry-academic cooperation.</p>
+                    <p class="my-3">The Department of Landscape Architecture, established in 1989, offers both bachelor's and master's degree programs. Located in the College of Arts Building, the department benefits from excellent teaching space and resources. It maintains high academic standards and admission rates, admitting international students since the 101 academic year. The undergraduate program requires 132 credits and a graduation design, while the master's program requires 34 credits and a thesis defense. The department's leadership has contributed to its professional diversity and richness in teaching.</p>
+                </div>
+                <a id="read-more-link-Art-lo" data-bs-toggle="collapse" href="#collapseArtLo" role="button" aria-expanded="false" aria-controls="collapseArtLo">Read more...</a>   
+                
+            </div>
+            <div class="col-md-6">
+                <img src="images/study-package/art/art3.jpg" class="container-fluid">
+            </div>
+        </div>
+    </div>
+
+    <div class="my-5" id="ff-art">
+        <div class="row align-items-start">
+            <div class="col-md-6">
+                <img src="images/study-package/art/art4.jpg" class="container-fluid">
+            </div>
+            <div class="col-md-6 justify-text">
+                <h3 class="my-3 fw-semibold">Faculty & Facilities</h3>
+                <p class="my-3 fw-semibold" style="font-size: 20px;">1. Outstanding Faculty</p>
+                <li class="mt-3 fw-semibold">Diverse Professional Backgrounds:</li>
+                <span class="mt-3">The faculty covers fields such as music, visual arts, and landscape design. For example, the Music Department's faculty includes internationally renowned musicians and educators who provide instruction in music theory, performance techniques, and composition.</span>
+                <li class="mt-3 fw-semibold">Extensive Practical Experience:</li>
+                <div class="collapse" id="collapseArtFF">
+                    <span class="mt-3"> Many faculty members have significant industry experience, combining theory and practice to offer students practical learning advice. For instance, the Applied Arts Department's faculty has extensive experience in animation, metalworking, and visual communication design, guiding students in their creative endeavors and participation in various design competitions.</span>
+                    <li class="mt-3 fw-semibold">Landscape Architecture Experts:</li>
+                    <span class="mt-3">The Landscape Architecture Department's faculty has backgrounds in landscape, architecture, urban planning, and more. For example, the current head, Professor Wen-Shan Huang, has profound expertise in landscape design and urban planning and actively promotes international exchanges and collaborations.</span>
+
+                    <p class="my-3 fw-semibold" style="font-size: 20px;">2. Advanced Facilities</p>
+                    <li class="mt-3 fw-semibold">Professional Studios and Laboratories:</li>
+                    <span class="mt-3">The college is equipped with specialized studios and labs that provide spaces for student creation and experimentation. For instance, the Landscape Architecture Department has design studios and model-making workshops where students can engage in hands-on design and fabrication.</span>
+                    <li class="mt-3 fw-semibold">High-Tech Equipment:</li>
+                    <span class="mt-3">The college boasts advanced technology such as 3D printers, digital recording studios, and multimedia classrooms, supporting students in various media and technical creations. For example, the digital recording studio in the Applied Arts Department provides professional environments for music and applied arts students for recording, mixing, and production.</span>
+
+                    <p class="my-3 fw-semibold" style="font-size: 20px;">3. Enhancing Global Competence and Innovation: A Spotlight on Fu Jen Catholic University's College of Arts</p>
+                    <li class="mt-3 fw-semibold">International Music Exchange Activities in the Music Department:</li>
+                    <span class="mt-3">The Music Department frequently hosts international masterclasses and workshops, inviting renowned musicians worldwide to guide students, enhancing their global perspectives and musical skills.</span>
+                    <li class="mt-3 fw-semibold">Interdisciplinary Collaboration in the Applied Arts Department:</li>
+                    <span class="mt-3">The Applied Arts Department collaborates with other faculties to offer interdisciplinary courses and workshops. For example, they partner with the Information Engineering Department to provide courses in digital interactive design, combining art and technology to expand students' creative horizons.</span>
+                    <li class="mt-3 fw-semibold">Practical Training in the Landscape Architecture Department: </li>
+                    <span class="mt-3">Students in the Landscape Architecture Department participate in real-world design and urban planning projects under faculty guidance and have opportunities to compete in international design competitions, enhancing their practical skills and global vision.</span>
+                </div>
+                <a id="read-more-link-Art-ff" data-bs-toggle="collapse" href="#collapseArtFF" role="button" aria-expanded="false" aria-controls="collapseArtFF">Read more...</a>
+            </div>
+        </div>
+    </div>
+
+    <div class="my-5" id="ow-art">
+        <div class="row align-items-start">
+            <div class="col-md-6 justify-text">
+                <h3 class="my-3 fw-semibold">Office Website</h3>
+                 <p class="my-3 fw-semibold" style="font-size: 18px;">College of Arts:</p>
+                 <a href=" https://www.finearts.fju.edu.tw/zh/index.html"> https://www.finearts.fju.edu.tw/zh/index.html</a>
+                 <p class="my-3 fw-semibold" style="font-size: 18px;">Department of Music:</p>
+                 <a href=" https://www.music.fju.edu.tw/"> https://www.music.fju.edu.tw/</a>
+                 <p class="my-3 fw-semibold" style="font-size: 18px;">Department of Applied Art:</p>
+                 <a href=" https://www.aart.fju.edu.tw/"> https://www.aart.fju.edu.tw/</a>
+                 <p class="my-3 fw-semibold" style="font-size: 18px;">Department of Landscape Architecture:</p>
+                 <a href=" https://www.landscape.fju.edu.tw/"> https://www.landscape.fju.edu.tw/</a>
+                 
+            </div>
+            <div class="col-md-6">
+                <img src="images/study-package/art/art5.jpg" class="container-fluid">
+            </div>
+        </div>
+    </div>
+
+    <div class="my-5">
+        <div class="row align-items-start">
+            <div class="col-md-6">
+               <img src="images/study-package/art/art6.jpg" class="container-fluid">  
+            </div>
+            <div class="col-md-6">
+               <img src="images/study-package/art/art7.jpg" class="container-fluid">
+            </div>
+        </div>
+    </div>
+
+    <div class="my-5">
+        <div class="row align-items-start">
+            <div class="col-md-6">
+               <img src="images/study-package/art/art8.jpg" class="container-fluid">  
+            </div>
+            <div class="col-md-6">
+               <img src="images/study-package/art/art9.jpg" class="container-fluid">
+            </div>
+        </div>
+    </div>
+
+    
+</div>
