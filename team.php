a32241c8009266c2348dc2ff01c3d74e029a1bb9
Justin <justin.official.2022@gmail.com>
2024-08-13 11:57:25 +0800

    開發團隊RWD完成

--- team.php
+++ team.php
@@ -23,141 +23,144 @@
     <title>Dev. Team</title>
 </head>
 <body>
-    <div class="vh-100 d-flex justify-content-center align-items-center">
-        <div class="row w-100 h-100 d-flex justify-content-center align-items-center" id="bg">
-            <span class="text-white" id="back-home">
-                <a href="home.php" class="text-decoration-none text-white">
-                    <img src="images/icons_back.png" alt="back">
-                    Back
-                </a>
-            </span>
+    <div class="min-vh-100">
+        <div class="d-flex flex-column h-100">
+            <div class="row my-3 ms-3">
+                <span class="fs-3 text-white">
+                    <a href="home.php" class="text-decoration-none text-white">
+                        <img src="images/icons_back.png" alt="back">
+                        Back
+                    </a>
+                </span>
+            </div>
 
-            <div class="col-12 d-flex justify-content-center align-items-center" id="content-card">
-                <div class="shadow d-flex justify-content-center align-items-center" id="title-card">
-                    <span class="fs-5 fw-semibold" id="title">開發團隊 Dev. Team</span>
+            <div class="container-fluid d-flex justify-content-between position-relative rounded my-5" id="content-card">
+                <div class="d-flex justify-content-center align-items-center position-absolute rounded shadow" id="title-card">
+                    <span class="fs-5 fw-semibold">開發團隊 Dev. Team</span>
                 </div>
 
-                <div class="container-fluid d-flex justify-content-between">
-                    <div class="row g-2 d-flex justify-content-center align-items-center">
-                        <div class="col-12 col-md-6 id-card">
-                            <div class="grid-item profile-card">
-                                <div class="profile-title">指導老師 Advisor</div>
-                                <div class="row gap-1 profile-body">
-                                    <div class="col-6 profile-photo">
-                                        <img src="" alt="Profile Photo">
-                                    </div>
-                                    <div class="col-6 contact-info">
-                                        <div class="d-flex flex-column justify-content-between">
-                                            <div>
-                                                <p class="fs-5 fw-semibold">杜逸寧</p>
-                                                <p class="fs-5 fw-semibold">Yining Tu</p>
-                                            </div>
-                                            <div>
-                                                <div class="d-flex flex-row gap-1">
-                                                    <i class="fas fa-project-diagram"></i>
-                                                    <div class="d-flex flex-column">
-                                                        <p>負責項目</p>
-                                                        <p>ENG</p>
-                                                    </div>
+                <div class="row g-2 d-flex justify-content-center align-items-center my-5">
+                    <div class="col-12 col-md-6">
+                        <div class="h-100 d-flex flex-column p-3">
+                            <div class="d-flex align-items-center fs-5 fw-bolder text-white mb-4 ps-3 rounded-pill profile-title">指導老師 Advisor</div>
+                            <div class="row d-flex justify-content-center align-items-center gap-2">
+                                <div class="col-12 col-sm-6 col-md-12 col-xl-6 profile-photo">
+                                    <img src="" alt="Profile Photo" class="rounded-circle">
+                                </div>
+                                <div class="col-12 col-sm-6 col-md-12 col-xl-6 contact-info">
+                                    <div class="d-flex flex-column justify-content-between">
+                                        <div class="text-center text-sm-start text-md-center text-xl-start mx-2">
+                                            <p class="fs-5 fw-semibold">杜逸寧</p>
+                                            <p class="fs-5 fw-semibold">Yining Tu</p>
+                                        </div>
+                                        <div class="mx-2">
+                                            <div class="d-flex flex-row gap-1">
+                                                <i class="fas fa-project-diagram"></i>
+                                                <div class="d-flex flex-column">
+                                                    <p>負責項目</p>
+                                                    <p>ENG</p>
                                                 </div>
-                                                <p class="d-flex flex-row gap-1">
-                                                    <i class="fas fa-envelope"></i>
-                                                    eniddu@gmail.com
-                                                </p>
                                             </div>
+                                            <p class="d-flex flex-row gap-1">
+                                                <i class="fas fa-envelope"></i>
+                                                eniddu@gmail.com
+                                            </p>
                                         </div>
                                     </div>
                                 </div>
                             </div>
                         </div>
-                        <div class="col-12 col-md-6 id-card">
-                            <div class="grid-item profile-card">
-                                    <div class="profile-title">組長 Team Leader</div>
-                                    <div class="row gap-1 profile-body">
-                                        <div class="col-6 profile-photo">
-                                            <img src="" alt="Profile Photo">
-                                        </div>
-                                        <div class="col-6 contact-info">
-                                            <div class="d-flex flex-column justify-content-between">
-                                                <div>
-                                                    <p class="fs-5 fw-semibold">陳郁雯</p>
-                                                    <p class="fs-5 fw-semibold">Yuwen Chen</p>
-                                                </div>
-                                                <div>
-                                                    <div class="d-flex flex-row gap-1">
-                                                        <i class="fas fa-project-diagram"></i>
-                                                        <div class="d-flex flex-column">
-                                                            <p>負責項目</p>
-                                                            <p>ENG</p>
-                                                        </div>
-                                                    </div>
-                                                    <p class="d-flex flex-row gap-1">
-                                                        <i class="fas fa-envelope"></i>
-                                                        a0985542655@gmail.com
-                                                    </p>
-                                                </div>
-                                            </div>
-                                        </div>
-                                    </div>
+                    </div>
+
+                    <div class="col-12 col-md-6">
+                        <div class="h-100 d-flex flex-column p-3">
+                            <div class="d-flex align-items-center fs-5 fw-bolder text-white mb-4 ps-3 rounded-pill profile-title">組長 Team Leader</div>
+                            <div class="row d-flex justify-content-center align-items-center gap-2">
+                                <div class="col-12 col-sm-6 col-md-12 col-xl-6 profile-photo">
+                                    <img src="" alt="Profile Photo" class="rounded-circle">
                                 </div>
-                            </div>
-                        <div class="col-12 col-md-6 id-card">
-                            <div class="grid-item profile-card">
-                                <div class="profile-title">團隊成員 Team Member</div>
-                                <div class="row gap-1 profile-body">
-                                    <div class="col-6 profile-photo">
-                                        <img src="images/team-members/justin.jpg" alt="Profile Photo">
-                                    </div>
-                                    <div class="col-6 contact-info">
-                                        <div class="d-flex flex-column justify-content-between">
-                                            <div>
-                                                <p class="fs-5 fw-semibold">郭政億</p>
-                                                <p class="fs-5 fw-semibold">Justin Kuo</p>
-                                            </div>
-                                            <div>
-                                                <div class="d-flex flex-row gap-1">
-                                                    <i class="fas fa-project-diagram"></i>
-                                                    <div class="d-flex flex-column">
-                                                        <p>負責項目</p>
-                                                        <p>ENG</p>
-                                                    </div>
+                                <div class="col-12 col-sm-6 col-md-12 col-xl-6 contact-info">
+                                    <div class="d-flex flex-column justify-content-between">
+                                        <div class="text-center text-sm-start text-md-center text-xl-start mx-2">
+                                            <p class="fs-5 fw-semibold">陳郁雯</p>
+                                            <p class="fs-5 fw-semibold">Yuwen Chen</p>
+                                        </div>
+                                        <div class="mx-2">
+                                            <div class="d-flex flex-row gap-1">
+                                                <i class="fas fa-project-diagram"></i>
+                                                <div class="d-flex flex-column">
+                                                    <p>負責項目</p>
+                                                    <p>ENG</p>
                                                 </div>
-                                                <p class="d-flex flex-row gap-1">
-                                                    <i class="fas fa-envelope"></i>
-                                                    sunshine.kuo2014@gmail.com
-                                                </p>
                                             </div>
+                                            <p class="d-flex flex-row gap-1">
+                                                <i class="fas fa-envelope"></i>
+                                                a0985542655@gmail.com
+                                            </p>
                                         </div>
                                     </div>
                                 </div>
                             </div>
                         </div>
-                        <div class="col-12 col-md-6 id-card">
-                            <div class="grid-item profile-card">
-                                <div class="profile-title">團隊成員 Team Member</div>
-                                <div class="row gap-1 profile-body">
-                                    <div class="col-6 profile-photo">
-                                        <img src="" alt="Profile Photo">
-                                    </div>
-                                    <div class="col-6 contact-info">
-                                        <div class="d-flex flex-column justify-content-between">
-                                            <div>
-                                                <p class="fs-5 fw-semibold">王孟蘋</p>
-                                                <p class="fs-5 fw-semibold">Menping Wang</p>
-                                            </div>
-                                            <div>
-                                                <div class="d-flex flex-row gap-1">
-                                                    <i class="fas fa-project-diagram"></i>
-                                                    <div class="d-flex flex-column">
-                                                        <p>負責項目</p>
-                                                        <p>ENG</p>
-                                                    </div>
+                    </div>
+
+                    <div class="col-12 col-md-6">
+                        <div class="h-100 d-flex flex-column p-3">
+                            <div class="d-flex align-items-center fs-5 fw-bolder text-white mb-4 ps-3 rounded-pill profile-title">團隊成員 Team Member</div>
+                            <div class="row d-flex justify-content-center align-items-center gap-2">
+                                <div class="col-12 col-sm-6 col-md-12 col-xl-6 profile-photo">
+                                    <img src="images/team-members/justin.jpg" alt="Profile Photo" class="rounded-circle">
+                                </div>
+                                <div class="col-12 col-sm-6 col-md-12 col-xl-6 contact-info">
+                                    <div class="d-flex flex-column justify-content-between">
+                                        <div class="text-center text-sm-start text-md-center text-xl-start mx-2">
+                                            <p class="fs-5 fw-semibold">郭政億</p>
+                                            <p class="fs-5 fw-semibold">Justin Kuo</p>
+                                        </div>
+                                        <div class="mx-2">
+                                            <div class="d-flex flex-row gap-1">
+                                                <i class="fas fa-project-diagram"></i>
+                                                <div class="d-flex flex-column">
+                                                    <p>負責項目</p>
+                                                    <p>ENG</p>
                                                 </div>
-                                                <p class="d-flex flex-row gap-1">
-                                                    <i class="fas fa-envelope"></i>
-                                                    dreamapple2003@gmail.com
-                                                </p>
                                             </div>
+                                            <p class="d-flex flex-row gap-1">
+                                                <i class="fas fa-envelope"></i>
+                                                sunshine.kuo2014@gmail.com
+                                            </p>
+                                        </div>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+
+                    <div class="col-12 col-md-6">
+                        <div class="h-100 d-flex flex-column p-3">
+                            <div class="d-flex align-items-center fs-5 fw-bolder text-white mb-4 ps-3 rounded-pill profile-title">團隊成員 Team Member</div>
+                            <div class="row d-flex justify-content-center align-items-center gap-2">
+                                <div class="col-12 col-sm-6 col-md-12 col-xl-6 profile-photo">
+                                    <img src="" alt="Profile Photo" class="rounded-circle">
+                                </div>
+                                <div class="col-12 col-sm-6 col-md-12 col-xl-6 contact-info">
+                                    <div class="d-flex flex-column justify-content-between">
+                                        <div class="text-center text-sm-start text-md-center text-xl-start mx-2">
+                                            <p class="fs-5 fw-semibold">王孟蘋</p>
+                                            <p class="fs-5 fw-semibold">Menping Wang</p>
+                                        </div>
+                                        <div class="mx-2">
+                                            <div class="d-flex flex-row gap-1">
+                                                <i class="fas fa-project-diagram"></i>
+                                                <div class="d-flex flex-column">
+                                                    <p>負責項目</p>
+                                                    <p>ENG</p>
+                                                </div>
+                                            </div>
+                                            <p class="d-flex flex-row gap-1">
+                                                <i class="fas fa-envelope"></i>
+                                                dreamapple2003@gmail.com
+                                            </p>
                                         </div>
                                     </div>
                                 </div>
