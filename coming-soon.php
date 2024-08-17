74d82ab3ffbd906a348da5b6c3c7f33fc20e8762
13822234 <sunshine.kuo2014@gmail.com>
2024-08-18 02:27:55 +0800

    主選單修正

--- coming-soon.php
+++ coming-soon.php
@@ -19,80 +19,17 @@
 
     <!-- CSS -->
     <link rel="stylesheet" href="css/fixed.css">
+    <link rel="stylesheet" href="css/menu.css">
     <link rel="stylesheet" href="css/coming-soon.css">
     
-    <title>Sitemap</title>
+    <title>Coming Soon</title>
    
 </head>
 <body>
     <?php include 'header.php'; ?>
 
     <!-- 選單 -->
-    <div class="offcanvas offcanvas-end d-block d-xl-none" tabindex="-1" id="offcanvasNavbar" aria-labelledby="offcanvasNavbarLabel">
-        <div class="offcanvas-header">
-            <h5 class="offcanvas-title fs-2 fw-bolder" id="offcanvasNavbarLabel">Menu</h5>
-            <button type="button" class="btn-close" data-bs-dismiss="offcanvas" aria-label="Close"></button>
-        </div>
-        <div class="offcanvas-body">
-            <ul class="navbar-nav justify-content-end flex-grow-1 pe-3">
-                <li class="nav-item fs-5 my-2">
-                    <a class="nav-link" href="https://bpis.fju.edu.tw/index.php">Fu Jen Global Academy</a>
-                </li>
-                <li class="nav-item fs-5 my-2">
-                    <a class="nav-link" href="coming-soon.php">Degree Students</a>
-                </li>
-                <li class="nav-item fs-5 my-2">
-                    <a class="nav-link" href="study-abroad.php">Study Abroad</a>
-                </li>
-                <li class="nav-item fs-5 my-2">
-                    <a class="nav-link" href="coming-soon.php">Chinese Student</a>
-                </li>
-                <li class="nav-item fs-5 my-2">
-                    <a class="nav-link" href="coming-soon.php">Enrollment Information</a>
-                </li>
-                <li class="nav-item fs-5 my-2">
-                    <a class="nav-link" href="contact-us.php">Contact Us</a>
-                </li>
-            </ul>
-        </div>
-    </div>
-
-    <nav class="navbar navbar-expand-lg bg-body-tertiary second-navbar">
-        <div class="container-fluid">
-            <img src="images/06-校徽-英文組合排列.png" id="logo">
-
-            <div class="d-flex ms-auto d-block d-xl-none">
-                <a href="how-to-apply.php" class="btn-bounce fw-semibold mx-5">Apply NOW !</a>
-                <a class="btn-no-style" data-bs-toggle="offcanvas" href="#offcanvasNavbar" role="button" aria-controls="offcanvasNavbar">
-                    <i class="fas fa-bars text-white"></i>
-                </a>
-            </div>
-
-            <ul class="navbar-nav ms-auto" id="second-list">
-                <div class="mx-5" id="applynow">
-                    <a href="how-to-apply.php" class="btn-bounce fw-semibold">Apply NOW !</a>
-                </div>
-                <li class="nav-item px-2">
-                    <a class="nav-link text-white" href="https://bpis.fju.edu.tw/index.php">Fu Jen Global Academy</a>
-                </li>
-                <li class="nav-item px-2">
-                    <a class="nav-link text-white" href="coming-soon.php">Degree Students</a>
-                </li>
-                <li class="nav-item px-2">
-                    <a class="nav-link text-white" href="study-abroad.php">Study Abroad</a>
-                </li>
-                <li class="nav-item px-2">
-                    <a class="nav-link text-white" href="coming-soon.php">Chinese Student</a>
-                </li>
-                <li class="nav-item px-2">
-                    <a class="nav-link text-white" href="coming-soon.php">Enrollment Information</a>
-                </li>
-                <li class="nav-item px-2">
-                    <a class="nav-link text-white" href="contact-us.php">Contact Us</a>
-                </li>
-            </ul>
-        </div>
-    </nav>
+    <?php include 'main-menu.php'?>
    
     <!-- Sitemap 標題 -->
      
---
+++ main-menu.php
@@ -0,0 +1,88 @@
+<div class="offcanvas offcanvas-end d-block d-xxl-none" tabindex="-1" id="offcanvasNavbar" aria-labelledby="offcanvasNavbarLabel">
+    <div class="offcanvas-header">
+        <h5 class="offcanvas-title fs-2 fw-bolder" id="offcanvasNavbarLabel">Menu</h5>
+        <button type="button" class="btn-close" data-bs-dismiss="offcanvas" aria-label="Close"></button>
+    </div>
+    <div class="offcanvas-body">
+        <ul class="navbar-nav justify-content-end flex-grow-1 pe-3">
+            <li class="nav-item fs-5 my-2">
+                <a class="nav-link" href="https://bpis.fju.edu.tw/index.php">Fu Jen Global Academy</a>
+            </li>
+            <li class="nav-item fs-5 my-2">
+                <a class="nav-link" href="coming-soon.php">Degree Students</a>
+            </li>
+            <li class="nav-item fs-5 my-2">
+                <a class="nav-link" href="study-abroad.php">Study Abroad</a>
+            </li>
+            <li class="nav-item fs-5 my-2">
+                <a class="nav-link" href="coming-soon.php">Exchange Student (Inbound)</a>
+            </li>
+            <li class="nav-item fs-5 my-2">
+                <a class="nav-link" href="coming-soon.php">Chinese Learning</a>
+            </li>
+            <li class="nav-item fs-5 my-2">
+                <a class="nav-link" href="coming-soon.php">Campus Life</a>
+            </li>
+            <li class="nav-item fs-5 my-2">
+                <a class="nav-link" href="contact-us.php">Contact Us</a>
+            </li>
+        </ul>
+    </div>
+</div>
+
+<nav class="navbar navbar-expand-lg bg-body-tertiary second-navbar">
+    <div class="container-fluid">
+        <img src="images/06-校徽-英文組合排列.png" id="logo">
+
+        <div class="d-flex ms-auto d-block d-xxl-none">
+            <a href="how-to-apply.php" class="btn-bounce fw-semibold mx-5">Apply NOW !</a>
+            <a class="btn-no-style" data-bs-toggle="offcanvas" href="#offcanvasNavbar" role="button" aria-controls="offcanvasNavbar">
+                <i class="fas fa-bars text-white"></i>
+            </a>
+        </div>
+
+        <ul class="navbar-nav ms-auto fs-5" id="second-list">
+            <div class="mx-5" id="applynow">
+                <a href="how-to-apply.php" class="btn-bounce fw-semibold">Apply NOW !</a>
+            </div>
+            <li class="nav-item px-2">
+                <a class="nav-link text-white" href="https://bpis.fju.edu.tw/index.php">Fu Jen Global Academy</a>
+            </li>
+            <li class="nav-item px-2">
+                <a class="nav-link text-white" href="coming-soon.php">Degree Students</a>
+            </li>
+            <li class="nav-item px-2">
+                <a class="nav-link text-white" href="study-abroad.php">Study Abroad</a>
+            </li>
+            <!-- <li class="nav-item px-2 dropdown">
+                <a class="nav-link text-white dropdown-toggle" href="study-abroad.php" id="dropdownMenuLink" data-bs-toggle="dropdown" aria-expanded="false">Study Abroad</a>
+                <ul class="dropdown-menu" aria-labelledby="dropdownMenuLink">
+                    <li>
+                        <a class="dropdown-item" href="study-package.php">Study Package</a>
+                    </li>
+                    <li>
+                        <a class="dropdown-item" href="taiwanese-culture.php">Taiwanese Culture</a>
+                    </li>
+                    <li>
+                        <a class="dropdown-item" href="#">Enterprise Visit</a>
+                    </li>
+                    <li>
+                        <a class="dropdown-item" href="Mandarin-Chinese-Learning.php">Mandarin Learing</a>
+                    </li>
+                </ul>
+            </li> -->
+            <li class="nav-item px-2">
+                <a class="nav-link text-white" href="coming-soon.php">Exchange Student (Inbound)</a>
+            </li>
+            <li class="nav-item px-2">
+                <a class="nav-link text-white" href="coming-soon.php">Chinese Learning</a>
+            </li>
+            <li class="nav-item px-2">
+                <a class="nav-link text-white" href="coming-soon.php">Campus Life</a>
+            </li>
+            <li class="nav-item px-2">
+                <a class="nav-link text-white" href="contact-us.php">Contact Us</a>
+            </li>
+        </ul>
+    </div>
+</nav>
