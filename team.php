c41a6eb0ea8114ec140fe93bce4de5fe0e1739ac
13822234 <sunshine.kuo2014@gmail.com>
2024-08-27 20:54:56 +0800

    team更新

---
+++ team.php
@@ -0,0 +1,180 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+
+    <!-- icon -->
+    <link href="images/logo.ico" rel="shortcut icon" />
+
+    <!-- 引入 Poppins 字體 -->
+    <link rel="stylesheet" href="https://fonts.googleapis.com/css2?family=Poppins:wght@400;600&display=swap">
+
+    <!-- Bootstrap -->
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-YvpcrYf0tY3lHB60NNkmXc5s9fDVZLESaAA55NDzOxhy9GkcIdslK1eN7N6jIeHz" crossorigin="anonymous"></script>
+
+    <!-- FontAwesome CSS -->
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.4.0/css/all.min.css">
+
+    <!-- CSS -->
+    <link rel="stylesheet" href="css/team.css">
+
+    <title>Website Developer</title>
+</head>
+<body>
+    <div class="min-vh-100">
+        <div class="d-flex flex-column h-100">
+            <div class="row my-3 ms-3">
+                <span class="fs-3 text-white">
+                    <a href="home.php" class="text-decoration-none text-white">
+                        <img src="images/icons_back.png" alt="back">
+                        Back
+                    </a>
+                </span>
+            </div>
+
+            <div class="container-fluid d-flex justify-content-between position-relative rounded my-5" id="content-card">
+                <div class="d-flex justify-content-center align-items-center position-absolute rounded shadow" id="title-card">
+                    <span class="fs-5 fw-semibold">Website Developer</span>
+                </div>
+
+                <div class="row g-2 d-flex justify-content-center align-items-center my-5">
+                    <div class="col-12 col-md-6">
+                        <div class="h-100 d-flex flex-column p-3">
+                            <div class="d-flex align-items-center justify-content-center ps-0 justify-content-sm-start ps-sm-3 justify-content-md-center ps-md-0 justify-content-xl-start ps-xl-3 fs-5 fw-bolder text-white mb-4 rounded-pill profile-title">指導老師 Advisor</div>
+                            <div class="row d-flex justify-content-center align-items-center gap-2">
+                                <div class="col-12 col-sm-6 col-md-12 col-xl-6 profile-photo">
+                                    <img src="images/team-members/Yi-Ning Tu.jpg" alt="Profile Photo" class="rounded-circle">
+                                </div>
+                                <div class="col-12 col-sm-6 col-md-12 col-xl-6 contact-info">
+                                    <div class="d-flex flex-column justify-content-between">
+                                        <div class="text-center text-sm-start text-md-center text-xl-start mx-2">
+                                            <p class="fs-5 fw-semibold">
+                                                杜逸寧
+                                                <a href="https://sites.google.com/view/yiningtu" target="_blank">
+                                                    <i class="fa fa-earth text-dark"></i>
+                                                </a>
+                                            </p>
+                                            <p class="fs-5 fw-semibold">Yi-Ning Tu</p>
+                                        </div>
+                                        <div class="mx-2">
+                                            <div class="d-flex flex-row gap-1">
+                                                <i class="fas fa-project-diagram"></i>
+                                                <div class="d-flex flex-column">
+                                                    <p>專案指導與監製</p>
+                                                    <p>Project Guidance and Supervision</p>
+                                                </div>
+                                            </div>
+                                            <p class="d-flex flex-row gap-1">
+                                                <i class="fas fa-envelope pt-1"></i>
+                                                eniddu@gmail.com
+                                            </p>
+                                        </div>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+
+                    <div class="col-12 col-md-6">
+                        <div class="h-100 d-flex flex-column p-3">
+                            <div class="d-flex align-items-center justify-content-center ps-0 justify-content-sm-start ps-sm-3 justify-content-md-center ps-md-0 justify-content-xl-start ps-xl-3 fs-5 fw-bolder text-white mb-4 rounded-pill profile-title">組長 Team Leader</div>
+                            <div class="row d-flex justify-content-center align-items-center gap-2">
+                                <div class="col-12 col-sm-6 col-md-12 col-xl-6 profile-photo">
+                                    <img src="images/team-members/yuwen.jpg" alt="Profile Photo" class="rounded-circle">
+                                </div>
+                                <div class="col-12 col-sm-6 col-md-12 col-xl-6 contact-info">
+                                    <div class="d-flex flex-column justify-content-between">
+                                        <div class="text-center text-sm-start text-md-center text-xl-start mx-2">
+                                            <p class="fs-5 fw-semibold">陳郁雯</p>
+                                            <p class="fs-5 fw-semibold">Yu-Wen Chen</p>
+                                        </div>
+                                        <div class="mx-2">
+                                            <div class="d-flex flex-row gap-1">
+                                                <i class="fas fa-project-diagram"></i>
+                                                <div class="d-flex flex-column">
+                                                    <p>專案管理、系統分析、前端開發</p>
+                                                    <p>Progrem Management, System Analyze, Front-End Development</p>
+                                                </div>
+                                            </div>
+                                            <p class="d-flex flex-row gap-1">
+                                                <i class="fas fa-envelope pt-1"></i>
+                                                a0985542655@gmail.com
+                                            </p>
+                                        </div>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+
+                    <div class="col-12 col-md-6">
+                        <div class="h-100 d-flex flex-column p-3">
+                            <div class="d-flex align-items-center justify-content-center ps-0 justify-content-sm-start ps-sm-3 justify-content-md-center ps-md-0 justify-content-xl-start ps-xl-3 fs-5 fw-bolder text-white mb-4 rounded-pill profile-title">團隊成員 Team Member</div>
+                            <div class="row d-flex justify-content-center align-items-center gap-2">
+                                <div class="col-12 col-sm-6 col-md-12 col-xl-6 profile-photo">
+                                    <img src="images/team-members/justin.jpg" alt="Profile Photo" class="rounded-circle">
+                                </div>
+                                <div class="col-12 col-sm-6 col-md-12 col-xl-6 contact-info">
+                                    <div class="d-flex flex-column justify-content-between">
+                                        <div class="text-center text-sm-start text-md-center text-xl-start mx-2">
+                                            <p class="fs-5 fw-semibold">郭政億</p>
+                                            <p class="fs-5 fw-semibold">Justin Kuo</p>
+                                        </div>
+                                        <div class="mx-2">
+                                            <div class="d-flex flex-row gap-1">
+                                                <i class="fas fa-project-diagram"></i>
+                                                <div class="d-flex flex-column">
+                                                    <p>前端開發、後端開發</p>
+                                                    <p>Front-End Development, Back-End Development</p>
+                                                </div>
+                                            </div>
+                                            <p class="d-flex flex-row gap-1">
+                                                <i class="fas fa-envelope pt-1"></i>
+                                                sunshine.kuo2014@gmail.com
+                                            </p>
+                                        </div>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+
+                    <div class="col-12 col-md-6">
+                        <div class="h-100 d-flex flex-column p-3">
+                            <div class="d-flex align-items-center justify-content-center ps-0 justify-content-sm-start ps-sm-3 justify-content-md-center ps-md-0 justify-content-xl-start ps-xl-3 fs-5 fw-bolder text-white mb-4 rounded-pill profile-title">團隊成員 Team Member</div>
+                            <div class="row d-flex justify-content-center align-items-center gap-2">
+                                <div class="col-12 col-sm-6 col-md-12 col-xl-6 profile-photo">
+                                    <img src="images/team-members/Mengpin.jpg" alt="Profile Photo" class="rounded-circle">
+                                </div>
+                                <div class="col-12 col-sm-6 col-md-12 col-xl-6 contact-info">
+                                    <div class="d-flex flex-column justify-content-between">
+                                        <div class="text-center text-sm-start text-md-center text-xl-start mx-2">
+                                            <p class="fs-5 fw-semibold">王孟蘋</p>
+                                            <p class="fs-5 fw-semibold">Meng-Pin Wang</p>
+                                        </div>
+                                        <div class="mx-2">
+                                            <div class="d-flex flex-row gap-1">
+                                                <i class="fas fa-project-diagram"></i>
+                                                <div class="d-flex flex-column">
+                                                    <p>系統分析、前端開發、後端開發、UI/UX</p>
+                                                    <p>System Analyze, Front-End Development, Back-End Development, UI/UX</p>
+                                                </div>
+                                            </div>
+                                            <p class="d-flex flex-row gap-1">
+                                                <i class="fas fa-envelope pt-1"></i>
+                                                dreamapple2003@gmail.com
+                                            </p>
+                                        </div>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+</body>
+</html>
