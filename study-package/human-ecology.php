be846763dd247f4eebf9bfe01c6e4412d7aedc15
yuwen9012 <156352342+yuwen9012@users.noreply.github.com>
2024-09-02 11:50:13 +0800

    學院網址跳轉新視窗

--- study-package/human-ecology.php
+++ study-package/human-ecology.php
@@ -289,7 +289,7 @@
 
     <div class="my-5" id="ow-ecology">
         <h3 class="my-3 fw-semibold">Office Website</h3>
-        <a href="https://www.he.fju.edu.tw/">https://www.he.fju.edu.tw/</a>
+        <a href="https://www.he.fju.edu.tw/" target="_blank">https://www.he.fju.edu.tw/</a>
     </div>
 
     <div class="my-5" id="ci-ecology">
