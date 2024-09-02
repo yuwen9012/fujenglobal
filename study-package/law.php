55e0d0ad54dbc1ec32a37b2896080e54143aa39e
yuwen9012 <156352342+yuwen9012@users.noreply.github.com>
2024-09-02 11:50:33 +0800

    學院網址跳轉新視窗

--- study-package/law.php
+++ study-package/law.php
@@ -171,7 +171,7 @@
 
     <div class="my-5" id="ow-law">
         <h3 class="my-3 fw-semibold">Office Website</h3>
-        <a href="http://www.ls.fju.edu.tw/">http://www.ls.fju.edu.tw/</a>
+        <a href="http://www.ls.fju.edu.tw/" target="_blank">http://www.ls.fju.edu.tw/</a>
     </div>
 
     <div class="my-5" id="ci-law">
