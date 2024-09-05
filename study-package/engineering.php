2600a50175777e09ac61eab607cf2271b90f16d2
dreamapple01 <dreamapple2003@gmail.com>
2024-09-05 15:54:08 +0800

    理工學院更新

--- study-package/engineering.php
+++ study-package/engineering.php
@@ -180,17 +180,18 @@
 
     <div class="my-5" id="ow-engineering">
         <h3 class="my-3 fw-semibold">Office Website</h3>
-        <a href="http://www.se.fju.edu.tw/" target="_blank">http://www.se.fju.edu.tw/</a>
+        <a href="http://en.se.fju.edu.tw/" target="_blank">http://en.se.fju.edu.tw/</a>
     </div>
 
     <div class="my-5" id="ci-engineering">
         <h3 class="my-3 fw-semibold">Contact Information</h3>
-        <p class="my-3">Grace Liu, Coordinator for International Development.</p>
-        <p class="my-3">E-mail: graceliu@mail.fju.edu.tw</p>
-        </br>
-        <p class="my-3 fw-semibold">Chief of Internationalization</p>
-        <p class="my-3">Tuan-Vinh Le</p>
+        <p class="mt-3 fw-semibold">Chief of Internationalization</p>
+        <p class="my-3">Assistant Professor,Tuan-Vinh Le</p>
+        <p class="my-3">Tel: 886-2-2905-6727</p>
         <p class="my-3">E-mail: 155315@mail.fju.edu.tw</p>
-        <p class="my-3">Tel: 886-2-29056727</p>
+        <p class="mt-3 fw-semibold">Secretary</p>
+        <p class="my-3">Grace Liu</p>
+        <p class="my-3">Tel:886-2-2905-3857</p>
+        <p class="my-3">E-mail:graceliu@mail.fju.edu.tw  </p>
     </div>
 </div>
