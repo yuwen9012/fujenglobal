84247cd05d39edc5d46c42d77a1ce9954a2bd3a1
yuwen9012 <156352342+yuwen9012@users.noreply.github.com>
2024-09-15 00:00:33 +0800

    忘記密碼後端

--- admin/js/forgetpassword.js
+++ admin/js/forgetpassword.js
@@ -1,10 +1,29 @@
- const togglePassword = document.getElementById('togglePassword');
- const password = document.getElementById('password');
+function forgetpassword(){
+    var account = document.getElementById("account").value;
+    var email = document.getElementById("email").value;
 
- togglePassword.addEventListener('click', function () {
-     const type = password.type === 'password' ? 'text' : 'password';
-     password.type = type;
-     
-     this.classList.toggle('fa-eye');
-     this.classList.toggle('fa-eye-slash');
- });
+    if (!account || !email) {
+        alert("請輸入帳號和信箱");
+        return;
+    }
+	alert("信件寄件中!");
+	$.ajax({
+		type:"POST",
+		url:"./php/forgetpassword.php",
+		data:{
+			"account":document.getElementById("account").value,
+			"email":document.getElementById("email").value,
+		},
+		success: function (data) {
+            console.log("data:", data);
+            alert(data);
+            if (data=="新密碼已成功寄送至您的信箱!") {
+                // 將用戶重新導向到登入頁面
+                window.location.href="./login.php";
+            }
+        },
+		error:function(){
+			alert("發生錯誤狀態:"+jqXHR.readyState+ "     "+jqXHR.status);
+		},
+	});
+}
