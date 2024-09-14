e5fd194a8603c71e682b328eae0a8c4bd85194d2
yuwen9012 <156352342+yuwen9012@users.noreply.github.com>
2024-09-14 20:57:29 +0800

    修改密碼後端

--- admin/js/changepassword.js
+++ admin/js/changepassword.js
@@ -13,3 +13,37 @@ const passwordFields = [
       this.classList.toggle('fa-eye-slash');
     });
   });
+
+function changePassword() {
+    var newPassword = document.getElementById("newPassword").value;
+    var confirmPassword = document.getElementById("confirmPassword").value;
+
+    // 檢查新密碼和確認新密碼是否相同
+    if (newPassword === confirmPassword) {
+        // 發送 Ajax 請求
+        $.ajax({
+            type: "POST",
+            url: "php/changepassword.php",
+            data: {
+                "oldPassword": document.getElementById("oldPassword").value,
+                "newPassword": document.getElementById("newPassword").value,
+            },
+            success: function (data) {
+                console.log("data:", data); // 打印响应内容
+                alert(data);
+                if (data=="密碼修改成功!請重新登入") {
+                    // 將用戶重新導向到登入頁面
+                    window.location.href="./login.php";
+                }
+            },
+            error: function (jqXHR) {
+                alert("發生錯誤狀態:" + jqXHR.readyState + "     " + jqXHR.status);
+            }
+        });
+    } else {
+        alert("新密碼與確認密碼不一致");
+    }
+}
+
+
+
