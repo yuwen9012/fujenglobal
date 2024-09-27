aaaab9bf3a0e71ff3b9b6f60f8a605a92622c08d
Justin <justin.official.2022@gmail.com>
2024-09-27 14:31:40 +0800

    sidebar.css改common.css

--- admin/enterprise-visit-setting.php
+++ admin/enterprise-visit-setting.php
@@ -25,7 +25,7 @@
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.5.1/css/all.min.css">
 
     <!-- CSS -->
-    <link rel="stylesheet" href="css/sidebar.css">
+    <link rel="stylesheet" href="css/common.css">
 
     <!-- JS -->
     <script type="text/javascript" src="js/sidebar.js"></script>
--- admin/study-abroad-setting.php
+++ admin/study-abroad-setting.php
@@ -25,7 +25,7 @@
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.5.1/css/all.min.css">
 
     <!-- CSS -->
-    <link rel="stylesheet" href="css/sidebar.css">
+    <link rel="stylesheet" href="css/common.css">
     <link rel="stylesheet" href="css/study-abroad-setting.css">
 
     <!-- JS -->
