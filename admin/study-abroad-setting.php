504a9134e04c1680ef592f72f0dc5ede2ec2160e
Justin <justin.official.2022@gmail.com>
2024-09-27 10:57:19 +0800

    修改錯字

--- admin/study-abroad-setting.php
+++ admin/study-abroad-setting.php
@@ -180,7 +180,7 @@
         <div class="modal-dialog modal-dialog-centered">
             <div class="modal-content">
                 <div class="modal-header">
-                    <h1 class="modal-title fs-5" id="editScorerModalLabel">編輯輪播牆圖片</h1>
+                    <h1 class="modal-title fs-5" id="editScorerModalLabel">編輯計分器資料</h1>
                     <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
                 </div>
                 <div class="modal-body">
