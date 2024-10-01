dee42a9c0ae28ce40be4bdd02b9028ecb42f8832
yuwen9012 <a0985542655@gmail.com>
2024-10-01 10:42:00 +0800

    overview初稿(測試)

---
+++ overview.php
@@ -0,0 +1,39 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+
+    <!-- icon -->
+    <link href="images/logo.ico" rel="shortcut icon" />
+
+    <!-- 引入 Poppins 字體 -->
+    <link rel="stylesheet" href="https://fonts.googleapis.com/css2?family=Poppins:wght@400;600&display=swap">
+
+    <!-- jQuery -->
+    <script src="https://code.jquery.com/jquery-3.5.1.min.js"></script>
+
+    <!-- Bootstrap -->
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-YvpcrYf0tY3lHB60NNkmXc5s9fDVZLESaAA55NDzOxhy9GkcIdslK1eN7N6jIeHz" crossorigin="anonymous"></script>
+
+    <!-- FontAwesome CSS -->
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.5.1/css/all.min.css">
+
+    <!-- Coin Slider -->
+    <script type="text/javascript" src="js/coin-slider.js"></script>
+    <link rel="stylesheet" href="css/coin-slider-styles.css">
+
+    <!-- CSS -->
+    <link rel="stylesheet" href="css/fixed.css">
+    <link rel="stylesheet" href="css/overview.css">
+
+    <title>Fu Jen Global</title>
+</head>
+<body>
+    <?php include 'header.php'; ?>
+    <div class="container">
+        <img src="images/overview.png" alt="overview">
+    </div>
+</body>
+</html>
