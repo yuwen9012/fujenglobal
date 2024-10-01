c15c6a45832c32ea78765618c425e271a5e0ddbb
yuwen9012 <a0985542655@gmail.com>
2024-10-01 20:44:36 +0800

    5-1架構完成(剩換圖/影片、RWD)

---
+++ js/overview.js
@@ -0,0 +1,24 @@
+document.addEventListener('DOMContentLoaded', function() {
+    const carouselElement = document.querySelector('#carouselExampleIndicators');
+    const carouselInstance = new bootstrap.Carousel(carouselElement);
+
+    window.showImage = function(src) {
+        const mainImage = document.querySelector('#mainImage');
+        mainImage.src = src;
+    };
+});
+
+function showImage(src) {
+    const carousel = document.querySelector('#carouselExampleIndicators');
+    const carouselItems = carousel.querySelectorAll('.carousel-item');
+    
+    carouselItems.forEach((item, index) => {
+        const images = item.querySelectorAll('img');
+        images.forEach(img => {
+            if (img.src.includes(src)) {
+                const carouselInstance = bootstrap.Carousel.getInstance(carousel);
+                carouselInstance.to(index); // 滑動到包含目標圖片的 carousel-item
+            }
+        });
+    });
+}
--- overview.php
+++ overview.php
@@ -24,16 +24,168 @@
     <script type="text/javascript" src="js/coin-slider.js"></script>
     <link rel="stylesheet" href="css/coin-slider-styles.css">
 
+     <!-- Bootstrap JS -->
+     <script src="https://stackpath.bootstrapcdn.com/bootstrap/5.1.3/js/bootstrap.bundle.min.js"></script>
+
     <!-- CSS -->
     <link rel="stylesheet" href="css/fixed.css">
     <link rel="stylesheet" href="css/overview.css">
+
+    <!-- JS -->
+    <script type="text/javascript" src="js/overview.js"></script>
 
     <title>Fu Jen Global</title>
 </head>
 <body>
     <?php include 'header.php'; ?>
-    <div class="container">
-        <img src="images/overview.png" alt="overview">
+    <!-- 藍色條 -->
+    <div class="container_blue">
     </div>
+    <!-- 上方大圖 -->
+    <div class="container1">
+        <img class="overview" src="images/overview.png" alt="overview">
+        <div class="text-overlay">Chinese-Learning</div>
+    </div>
+    <!-- Overview -->
+    <div class="container2">
+        <div class="row justify-content-center">
+            <div class="col-8">
+                <h3>Overview</h3>
+                <p>The Center of Chinese Language and Culture of Fu Jen Catholic University was founded originally by Divine World Missionaries. For fathers, brothers and being specialized in Mandarin was not just crucial for preaching, but especially for lecturing and managing the school. Hence, the idea of establishing a Chinese-Learning Center was directed by the President Cardinal Paul Yu Pin in 1964.</p>
+            </div>
+        </div>
+    </div>
+    <!-- 4個icon -->
+    <div class="container3">
+        <div class="row justify-content-center">
+            <div class="col-lg-3 col-sm-12">
+                <img class="icon" src="images/overview_icon1.png" alt="Small classes">
+                <p class="icon-label">Small classes</p>
+            </div>
+            <div class="col-lg-3 col-sm-12">
+                <img class="icon" src="images/overview_icon2.png" alt="Zhuyin">
+                <p class="icon-label">Zhuyin&Pinyi<br>Traditional & Simplified</p>
+            </div>
+            <div class="col-lg-3 col-sm-12">
+                <img class="icon" src="images/overview_icon3.png" alt="Tutorial">
+                <p class="icon-label">Tutorial</p>
+            </div>
+            <div class="col-lg-3 col-sm-12">
+                <img class="icon" src="images/overview_icon4.png" alt="Counseling and Caring">
+                <p class="icon-label">Counseling and Caring</p>
+            </div>
+        </div>
+    </div>
+    <!-- 左文字右影片區 -->
+    <div class="container4">
+        <div class="row justify-content-center">
+            <div class="col-lg-6 col-sm-12">
+                <li>Effective interaction between teachers and students with no more than 7 students.</li>
+                <br>
+                <li>Different Mandarin Chinese systems and materials are available to tailor students’ learning needs.</li>
+                <br>
+                <li>Support your learning by providing individual or a small group (less than 5 students) tutorial session.</li>
+                <br>
+                <li>Professional counseling services for students adjusting to life in Taiwan and dealing with difficult situations.</li>
+            </div>
+            <div class="video-container col-lg-6 col-sm-12">
+                <iframe
+                    width="100%"
+                    height="100%"
+                    src="https://www.youtube.com/embed/6h5yv5A1EW0?autoplay=1&mute=1&loop=1&playlist=6h5yv5A1EW0"
+                    title="YouTube video player"
+                    frameborder="0"
+                    allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share"
+                    allowfullscreen
+                ></iframe>
+            </div>
+        </div>
+    </div>
+    <!-- 輪播牆 -->
+    <div class="container5">
+           <div class="row">
+                <div class="col-lg-3 col-md-2">
+                </div>
+                <div class="col-lg-6 col-md-8 col-5">
+                    <h3>Facilities</h3>
+                    <p>Language Center facilities include air-conditioned classrooms, audio-visual rooms, speech laboratory, multimedia equipped classrooms, study hall, and cross-cultural learning space.</p>
+                    <img  class="mainImage" id="mainImage" src="images/why1.jpg" alt="Facilities" class="img-fluid">
+                </div>
+                <div class="col-lg-3 col-md-2">
+                </div>
+           </div>
+
+           <div class="row">
+               <div class="col-lg-3 col-md-2"></div>
+               <div class="col-lg-6 col-md-8 col-5 justify-content-center align-items-center">
+                   <div id="carouselExampleIndicators" class="carousel slide">
+                       <div class="carousel-inner">
+                           <div class="carousel-item active">
+                               <div class="d-flex">
+                                   <img src="images/why1.jpg" alt="..." onclick="showImage('images/why1.jpg')" style="cursor: pointer;">
+                                   <img src="images/Scholar2.jpg" alt="..." onclick="showImage('images/Scholar2.jpg')" style="cursor: pointer;">
+                                   <img src="images/Scholar3.jpg" alt="..." onclick="showImage('images/Scholar3.jpg')" style="cursor: pointer;">
+                                   <img src="images/Scholar4.jpg" alt="..." onclick="showImage('images/Scholar4.jpg')" style="cursor: pointer;">
+                                   <img src="images/Scholar5.jpg" alt="..." onclick="showImage('images/Scholar5.jpg')" style="cursor: pointer;">
+                               </div>
+                           </div>
+                           <div class="carousel-item ">
+                               <div class="d-flex">
+                                   <img src="images/Scholar5.jpg" alt="..." onclick="showImage('images/Scholar5.jpg')" style="cursor: pointer;">
+                                   <img src="images/Scholar6.jpg" alt="..." onclick="showImage('images/Scholar6.jpg')" style="cursor: pointer;">
+                                   <img src="images/Scholar7.jpg" alt="..." onclick="showImage('images/Scholar7.jpg')" style="cursor: pointer;">
+                                   <img src="images/Scholar8.jpg" alt="..." onclick="showImage('images/Scholar8.jpg')" style="cursor: pointer;">
+                                   <img src="images/Scholar9.jpg" alt="..." onclick="showImage('images/Scholar9.jpg')" style="cursor: pointer;">
+                               </div>
+                           </div>
+                           <div class="carousel-item ">
+                               <div class="d-flex">
+                                   <img src="images/Scholar9.jpg" alt="..." onclick="showImage('images/Scholar9.jpg')" style="cursor: pointer;">
+                                   <img src="images/Scholar10.jpg" alt="..." onclick="showImage('images/Scholar10.jpg')" style="cursor: pointer;">
+                                   <img src="images/Scholar5.jpg" alt="..." onclick="showImage('images/Scholar5.jpg')" style="cursor: pointer;">
+                                   <img src="images/Scholar11.jpg" alt="..." onclick="showImage('images/Scholar11.jpg')" style="cursor: pointer;">
+                                   <img src="images/Scholar1.jpg" alt="..." onclick="showImage('images/Scholar1.jpg')" style="cursor: pointer;">
+                               </div>
+                           </div>
+                       </div>
+                       <button class="carousel-control-prev" type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide="prev">
+                           <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+                           <span class="visually-hidden">Previous</span>
+                       </button>
+                       <button class="carousel-control-next" type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide="next">
+                           <span class="carousel-control-next-icon" aria-hidden="true"></span>
+                           <span class="visually-hidden">Next</span>
+                       </button>
+                   </div>
+               </div>
+               <div class="col-lg-3 col-md-2"></div>
+           </div>
+           <br>
+    </div>
+    <!-- Other Information -->
+    <div class="container6">
+        <div class="row justify-content-center">
+            <div class="col-8">
+                <h3>Other Information</h3>
+                <p class="othertext">Language Center students have access to other university resources such as the libraries, computer and multimedia laboratories, sports facilities (e.g., swimming pool), and medical clinic at the university. </p>
+            </div>
+        </div>
+    </div>
+    <div class="container">
+        <div class="row justify-content-center">
+            <div class="col-1">
+                <a href="why-FJCU.php" class="fs-3 fw-semibold"><img src="images/overview_button.png" class="img-icon mx-3" alt="why-FJCU icon"></a>
+            </div>
+        </div>
+    </div>
+    <div class="container6">
+        <div class="row justify-content-center">
+            <div class="col-8">
+                <p class="ps">＊Fu Jen University has one of the most beautiful campuses in Taiwan. With lots of open green areas, students can easily find places to rest and recreate. A post office, ATM machines, cafeterias, coffee shops, convenient stores and bookstores are easily accessible on campus.</p>
+            </div>
+        </div>
+    </div>
+
+    <?php include 'footer.php'; ?>
 </body>
 </html>
